8324578c653303fbfb452489fd6147a7665a7ec6
matiushariman <matius.hariman@gmail.com>
2023-01-15 15:32:59 +0700

    update AboutMe

--- apps/website/components/AboutMe/AboutMe.tsx
+++ apps/website/components/AboutMe/AboutMe.tsx
@@ -1,15 +1,69 @@
+import { GOJEK_URL, BAMBU_URL, LAYOFF_NEWS_URL } from '../../constants/URL';
+
 export function AboutMe() {
   return (
     <div className="space-y-3 p-4">
       <h2 className="text-gold text-xl font-bold">About Me</h2>
-      <p className="text-sm md:text-justify">
-        A seasoned frontend engineer with years of experience building
-        state-of-the-art web applications, I am passionate about delivering
-        modern user experience in the most efficient way possible. Experienced
-        in translating user requirements to create scalable applications, and
-        proven track record of thriving in a fast-paced agile environment. TLDR:
-        Front-end Ninja.
-      </p>
+      <div className="space-y-2">
+        <p className="text-sm md:text-justify">
+          I started my web development journey in 2016 with{' '}
+          <a
+            href={BAMBU_URL}
+            className="text-primary font-bold underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            BAMBU
+          </a>
+          , where I was fortunate enough to be part of the co-founding members
+          of the awards winning team on a mission to provide next-gen wealth
+          management solutions for financial institutions worldwide.
+        </p>
+        <p className="text-sm md:text-justify">
+          After almost 6 years worth of rewarding adventure in the startup
+          world, I moved onto my next journey to build digital economy of the
+          future in one of the world's largest economies with{' '}
+          <a
+            href={GOJEK_URL}
+            className="text-primary font-bold underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Gojek
+          </a>
+          . Unfortunately,{' '}
+          <a
+            href={LAYOFF_NEWS_URL}
+            className="text-primary font-bold underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            my venture was cut short along with 1,299 others
+          </a>{' '}
+          due to the macroeconomic situation. While looking for the next big
+          thing, I spend most of my days learning new things, building side
+          projects, and of course, catching the Celtics game whenever I can. Go
+          C's!
+        </p>
+        <div className="space-y-2">
+          <p className="text-sm md:text-justify">
+            Some of the technologies that I've truly enjoyed working with
+            recently:
+          </p>
+          <div className="grid grid-cols-2 md:grid-cols-3">
+            <ul className="list-inside list-disc text-sm">
+              <li>Typescript</li>
+              <li>NextJS</li>
+              <li>NX</li>
+            </ul>
+            <ul className="list-inside list-disc text-sm">
+              <li>ReactJS</li>
+              <li>Tailwind</li>
+              <li>Playwright</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ apps/website/constants/URL.ts
@@ -0,0 +1,5 @@
+export const GOJEK_URL = 'https://www.gojek.com/en-id/';
+export const BAMBU_URL = 'https://bambu.co/';
+
+export const LAYOFF_NEWS_URL =
+  'https://www.channelnewsasia.com/business/indonesia-goto-cut-1300-jobs-gojek-retrench-tech-firms-ride-hailing-3083101';
