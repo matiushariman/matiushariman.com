5d50b54db34785d32b77909383a1c7a7a4a45823
matiushariman <matius.hariman@gmail.com>
2023-01-17 00:48:06 +0700

    make theme switch accessible by screen reader

--- apps/website/components/ThemeSwitch/ThemeSwitch.tsx
+++ apps/website/components/ThemeSwitch/ThemeSwitch.tsx
@@ -4,10 +4,18 @@ import Moon from '../Icons/Moon/Moon';
 
 export function ThemeSwitch() {
   const { toggleMode, mode } = useThemeContext();
+  const isLightMode = mode === 'light';
 
   return (
-    <button onClick={toggleMode}>
-      {mode === 'light' ? <Sun /> : <Moon />}
+    <button
+      onClick={toggleMode}
+      aria-label={
+        isLightMode
+          ? 'So bright! I want to switch to dark mode'
+          : 'So dark! I want to switch to light mode'
+      }
+    >
+      {isLightMode ? <Sun /> : <Moon />}
     </button>
   );
 }
