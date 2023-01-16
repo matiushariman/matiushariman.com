a51df81701ec6667829a0cfbcb7571e932e56108
matiushariman <matius.hariman@gmail.com>
2023-01-16 11:03:48 +0700

    add GTM

---
+++ apps/website/pages/_document.tsx
@@ -0,0 +1,58 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5TV3K3X');`,
+          }}
+        />
+        <meta name="theme-color" content="#007a33" />
+        <meta
+          name="description"
+          content="A seasoned frontend engineer, and an avid Boston Celtics fan based in sunny Singapore."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5TV3K3X"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>`,
+          }}
+        />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
