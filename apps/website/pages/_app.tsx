703211cf240d99598465f1cc972a70b6beced6db
matiushariman <matius.hariman@gmail.com>
2023-01-16 09:25:51 +0700

    add favicon

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -11,7 +11,30 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>matiushariman.com</title>
+        <title>Matius Hariman</title>
+        <meta name="theme-color" content="#007a33" />
+        <meta
+          name="description"
+          content="A seasoned frontend engineer, and an avid Boston Celtics fan based in sunny Singapore."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <ThemeProvider>
         <ThemeContext.Consumer>
