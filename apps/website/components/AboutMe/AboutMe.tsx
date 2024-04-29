69506c7750b767bd9841f216d4ec3066ef35bf7e
matiushariman <matius.hariman@gmail.com>
2024-04-29 21:36:50 +0800

    updated about me

--- apps/website/components/AboutMe/AboutMe.tsx
+++ apps/website/components/AboutMe/AboutMe.tsx
@@ -1,69 +1,126 @@
-import { GOJEK_URL, BAMBU_URL, LAYOFF_NEWS_URL } from '../../constants/URL';
+import {
+  GOJEK_URL,
+  BAMBU_URL,
+  LAYOFF_NEWS_URL,
+  BAMBU_GO_NEWS_URL,
+  BAMBU_SHUTDOWN_URL,
+  YOUTRIP_URL,
+  YOUBIZ_PR_URL,
+} from '../../constants/URL';
 
 export function AboutMe() {
   return (
-    <div className="space-y-3 p-4">
+    <div className="space-y-4 p-4">
       <h2 className="text-gold-dark dark:text-gold text-xl font-bold">
         About Me
       </h2>
-      <div className="space-y-2">
-        <p className="text-sm md:text-justify">
-          I started my web development journey in 2016 with{' '}
-          <a
-            href={BAMBU_URL}
-            className="text-primary font-bold underline"
-            target="_blank"
-            rel="noreferrer"
-          >
-            BAMBU
-          </a>
-          , where I was fortunate enough to be part of the co-founding members
-          of the awards winning team on a mission to provide next-gen wealth
-          management solutions for financial institutions worldwide.
-        </p>
-        <p className="text-sm md:text-justify">
-          After almost 6 years worth of rewarding adventure in the startup
-          world, I moved onto my next journey to build digital economy of the
-          future in one of the world's largest economies with{' '}
-          <a
-            href={GOJEK_URL}
-            className="text-primary font-bold underline"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Gojek
-          </a>
-          . Unfortunately,{' '}
-          <a
-            href={LAYOFF_NEWS_URL}
-            className="text-primary font-bold underline"
-            target="_blank"
-            rel="noreferrer"
-          >
-            my venture was cut short along with 1,299 others
-          </a>{' '}
-          due to the macroeconomic situation. While looking for the next big
-          thing, I spend most of my days learning new things, building side
-          projects, and of course, catching the Celtics game whenever I can. Go
-          C's!
-        </p>
+      <div className="space-y-3">
         <div className="space-y-2">
+          <h3 className="text-gold-dark dark:text-gold text-md font-bold">
+            The Humble Beginning
+          </h3>
           <p className="text-sm md:text-justify">
-            Some of the technologies that I've truly enjoyed working with
-            recently:
+            My journey in tech began in 2016, where I had privilege to be part
+            of the co-founding members of the awards winning team at{' '}
+            <a
+              href={BAMBU_URL}
+              className="text-primary font-bold underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              BAMBU (closed in 2023)
+            </a>{' '}
+            on a mission to provide next-gen wealth management solutions for
+            financial institutions worldwide.
           </p>
-          <div className="grid grid-cols-2 md:grid-cols-3">
-            <ul className="list-inside list-disc text-sm">
-              <li>Typescript</li>
-              <li>NextJS</li>
-              <li>NX</li>
-            </ul>
-            <ul className="list-inside list-disc text-sm">
-              <li>ReactJS</li>
-              <li>Tailwind</li>
-              <li>Playwright</li>
-            </ul>
-          </div>
+        </div>
+        <div className="space-y-2">
+          <h3 className="text-gold-dark dark:text-gold text-md font-bold">
+            The Storm
+          </h3>
+          <p className="text-sm md:text-justify">
+            After almost 6 years worth of rewarding adventure in the startup
+            world, I moved onto my next journey to build digital economy of the
+            future in one of the world&apos;s largest economies with{' '}
+            <a
+              href={GOJEK_URL}
+              className="text-primary font-bold underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Gojek
+            </a>
+            . Unfortunately,{' '}
+            <a
+              href={LAYOFF_NEWS_URL}
+              className="text-primary font-bold underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              my venture was cut short along with 1,299 others
+            </a>{' '}
+            due to the macroeconomic situation. While looking for the next big
+            thing, I spend most of my days learning new things, building side
+            projects, and of course, catching the Celtics game whenever I can.
+            Go C&apos;s!
+          </p>
+        </div>
+        <div className="space-y-2">
+          <h3 className="text-gold-dark dark:text-gold text-md font-bold">
+            Returning To The Roots
+          </h3>
+          <p className="text-sm md:text-justify">
+            A full-circle moment happened, it&apos;s the long (or short) awaited
+            reunion with my colleagues and bosses at BAMBU (closed in 2023). Now
+            wiser and renewed, I was appointed as the lead engineer of
+            BAMBU&apos;s flagship product:{' '}
+            <a
+              href={BAMBU_GO_NEWS_URL}
+              className="text-primary font-bold underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              BAMBU GO, the first ever SaaS Robo-advisor
+            </a>
+            . Unfortunately, it didn&apos;t get the fairytale ending that it
+            deserved (
+            <a
+              href={BAMBU_SHUTDOWN_URL}
+              className="text-primary font-bold underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              BAMBU closed down their operation in 2023
+            </a>
+            ).
+          </p>
+        </div>
+        <div className="space-y-2">
+          <h3 className="text-gold-dark dark:text-gold text-md font-bold">
+            The Story Continues
+          </h3>
+          <p className="text-sm md:text-justify">
+            I&apos;m currently working as Senior Software Engineer at{' '}
+            <a
+              href={YOUTRIP_URL}
+              className="text-primary font-bold underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              YouTrip
+            </a>{' '}
+            as part of the{' '}
+            <a
+              href={YOUBIZ_PR_URL}
+              className="text-primary font-bold underline"
+              target="_blank"
+              rel="noreferrer"
+            >
+              YouBiz
+            </a>{' '}
+            team . Where I&apos;m currently tasked to develop an enterprise
+            level multi-currency expense management platform: YouBiz.
+          </p>
         </div>
       </div>
     </div>
--- apps/website/constants/URL.ts
+++ apps/website/constants/URL.ts
@@ -4,8 +4,13 @@ export const BAMBU_URL = 'https://bambu.co/';
 export const YOUTRIP_URL = 'https://www.you.co/sg/';
 
 export const LAYOFF_NEWS_URL =
-  'https://www.channelnewsasia.com/business/indonesia-goto-cut-1300-jobs-gojek-retrench-tech-firms-ride-hailing-3083101';
+  'https://www.straitstimes.com/business/indonesia-s-goto-cuts-1300-workers-or-12-of-total-employees';
 
 export const GITHUB_PROFILE_URL = 'https://github.com/matiushariman';
 export const LINKEDIN_PROFILE_URL =
   'https://www.linkedin.com/in/matiushariman/';
+export const BAMBU_GO_NEWS_URL =
+  'https://www.hubbis.com/news/bambu-launches-groundbreaking-saas-based-robo-advisor-during-the-singapore-fintech-festival-2023-and-announces-partnership-with-wealthkernel-in-the-uk';
+export const BAMBU_SHUTDOWN_URL =
+  'https://www.techinasia.com/sg-roboadvisory-firm-closed-2023end-missing-profitability-target-founder';
+export const YOUBIZ_PR_URL = 'https://www.you.co/sg/blog/introducing-youbiz/';
